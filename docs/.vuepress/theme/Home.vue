8bf244234fbb4aa8073c7a11e438fa9dba41ebb1
hpstream <hp915414300@gmail.com>
2019-05-29 14:39:33 +0800

    修改样式

--- docs/.vuepress/theme/Home.vue
+++ docs/.vuepress/theme/Home.vue
@@ -7,11 +7,15 @@
         alt="hero"
       >
 
-      <h1>{{ data.heroText || $title || 'Hello' }}</h1>
-
-      <p class="description">
-        {{ data.tagline || $description || 'Welcome to your VuePress site' }}
-      </p>
+      <div
+        v-if='data.motto'
+        class='motto'
+      >
+        <div class='mottotitle'>座右铭:</div>
+        <div class="description">
+          <p v-for='item of data.motto'>{{item.info}}</p>
+        </div>
+      </div>
 
       <p
         class="action"
@@ -38,7 +42,7 @@
       </div>
     </div>
 
-    <Content custom/>
+    <Content custom />
 
     <div
       class="footer"
@@ -50,114 +54,191 @@
 </template>
 
 <script>
-import NavLink from './NavLink.vue'
+import NavLink from "./NavLink.vue";
 
 export default {
   components: { NavLink },
 
   computed: {
-    data () {
-      return this.$page.frontmatter
+    data() {
+      return this.$page.frontmatter;
     },
 
-    actionLink () {
+    actionLink() {
       return {
         link: this.data.actionLink,
         text: this.data.actionText
+      };
+    }
+  },
+  mounted() {
+    console.log(this.$page.frontmatter);
+  }
+};
+</script>
+
+<style lang="stylus">
+@import './styles/config.styl';
+
+.home {
+  padding: $navbarHeight 2rem 0;
+  max-width: 960px;
+  min-height: 100vh;
+  background-color: #f5f5d5;
+  font-family: Georgia, serif;
+  letter-spacing: -0.01em;
+  margin: 0px auto;
+
+  .motto {
+    text-align: left;
+
+    .mottotitle {
+      font-size: 2rem;
+      color: #444;
+    }
+
+    p {
+      margin: 0;
+    }
+  }
+
+  .hero {
+    text-align: center;
+
+    img {
+      max-width: 100%;
+      max-height: 280px;
+      display: block;
+      margin: 3rem auto 1.5rem;
+    }
+
+    h1 {
+      font-size: 3rem;
+    }
+
+    h1, .description, .action {
+      margin: 1.8rem auto;
+    }
+
+    .description {
+      font-size: 1.6rem;
+      color: #b2b2b2;
+      text-indent: 2em;
+    }
+
+    .action-button {
+      display: inline-block;
+      font-size: 1.2rem;
+      color: #fff;
+      background-color: $accentColor;
+      padding: 0.8rem 1.6rem;
+      border-radius: 4px;
+      transition: background-color 0.1s ease;
+      box-sizing: border-box;
+      border-bottom: 1px solid darken($accentColor, 10%);
+
+      &:hover {
+        background-color: lighten($accentColor, 10%);
+      }
+    }
+  }
+
+  .features {
+    border-top: 1px solid $borderColor;
+    padding: 1.2rem 0;
+    margin-top: 2.5rem;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: flex-start;
+    align-content: stretch;
+    justify-content: space-between;
+  }
+
+  .feature {
+    flex-grow: 1;
+    flex-basis: 30%;
+    max-width: 30%;
+
+    h2 {
+      font-size: 1.4rem;
+      font-weight: 500;
+      border-bottom: none;
+      padding-bottom: 0;
+      color: lighten($textColor, 10%);
+    }
+
+    p {
+      color: lighten($textColor, 25%);
+    }
+  }
+
+  .footer {
+    padding: 2.5rem;
+    border-top: 1px solid $borderColor;
+    text-align: center;
+    color: lighten($textColor, 25%);
+  }
+}
+
+@media (max-width: $MQMobile) {
+  .home {
+    .features {
+      flex-direction: column;
+    }
+
+    .feature {
+      max-width: 100%;
+      padding: 0 2.5rem;
+    }
+  }
+}
+
+@media (max-width: $MQMobileNarrow) {
+  .home {
+    padding-left: 1.5rem;
+    padding-right: 1.5rem;
+
+    .hero {
+      img {
+        max-height: 210px;
+        margin: 2rem auto 1.2rem;
+      }
+
+      h1 {
+        font-size: 2rem;
+      }
+
+      h1, .description, .action {
+        margin: 1.2rem auto;
+      }
+
+      .motto {
+        .mottotitle {
+          margin-top: 1rem;
+          font-size: 1.6rem;
+        }
+      }
+
+      .description {
+        margin-top: 0px;
+        font-size: 1rem;
+
+        p {
+          margin: 0;
+        }
+      }
+
+      .action-button {
+        font-size: 1rem;
+        padding: 0.6rem 1.2rem;
+      }
+    }
+
+    .feature {
+      h2 {
+        font-size: 1.25rem;
       }
     }
   }
 }
-</script>
-
-<style lang="stylus">
-@import './styles/config.styl'
-
-.home
-  padding $navbarHeight 2rem 0
-  max-width 960px
-  margin 0px auto
-  .hero
-    text-align center
-    img
-      max-width 100%
-      max-height 280px
-      display block
-      margin 3rem auto 1.5rem
-    h1
-      font-size 3rem
-    h1, .description, .action
-      margin 1.8rem auto
-    .description
-      max-width 35rem
-      font-size 1.6rem
-      line-height 1.3
-      color lighten($textColor, 40%)
-    .action-button
-      display inline-block
-      font-size 1.2rem
-      color #fff
-      background-color $accentColor
-      padding 0.8rem 1.6rem
-      border-radius 4px
-      transition background-color .1s ease
-      box-sizing border-box
-      border-bottom 1px solid darken($accentColor, 10%)
-      &:hover
-        background-color lighten($accentColor, 10%)
-  .features
-    border-top 1px solid $borderColor
-    padding 1.2rem 0
-    margin-top 2.5rem
-    display flex
-    flex-wrap wrap
-    align-items flex-start
-    align-content stretch
-    justify-content space-between
-  .feature
-    flex-grow 1
-    flex-basis 30%
-    max-width 30%
-    h2
-      font-size 1.4rem
-      font-weight 500
-      border-bottom none
-      padding-bottom 0
-      color lighten($textColor, 10%)
-    p
-      color lighten($textColor, 25%)
-  .footer
-    padding 2.5rem
-    border-top 1px solid $borderColor
-    text-align center
-    color lighten($textColor, 25%)
-
-@media (max-width: $MQMobile)
-  .home
-    .features
-      flex-direction column
-    .feature
-      max-width 100%
-      padding 0 2.5rem
-
-@media (max-width: $MQMobileNarrow)
-  .home
-    padding-left 1.5rem
-    padding-right 1.5rem
-    .hero
-      img
-        max-height 210px
-        margin 2rem auto 1.2rem
-      h1
-        font-size 2rem
-      h1, .description, .action
-        margin 1.2rem auto
-      .description
-        font-size 1.2rem
-      .action-button
-        font-size 1rem
-        padding 0.6rem 1.2rem
-    .feature
-      h2
-        font-size 1.25rem
 </style>
--- docs/README.md
+++ docs/README.md
@@ -1,14 +1,11 @@
 ---
 home: true
-heroImage: /img/logo.jpeg
-actionText: 快速上手 →
-actionLink: /zh/guide/
-features:
-- title: 简洁至上
-  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
-- title: Vue驱动
-  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
-- title: 高性能
-  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
+# heroImage: /img/logo.jpeg
+# actionText: 快速上手 →
+# actionLink: /zh/guide/
+motto:
+- info: 昨夜西风凋碧树，独上高楼，望尽天涯路。
+- info: 衣带渐宽终不悔，为伊消得人憔悴。
+- info: 众里寻他千百度，蓦然回首，那人正在灯火阑珊处。
 footer: MIT Licensed | Copyright © 2018-present Evan You
 ---
