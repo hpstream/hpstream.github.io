d8a9340763b31caabaa1ca5a38d3232fc7d6a08c
hpstream <915414300@qq.com>
2019-09-28 00:51:14 +0800

    dd

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:hpstream/hpstream.github.io.git master
+git push -f git@github.com:hpstream/hpstream.github.io.git 
 # git@github.com:hpstream/hpstream.github.io.git
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
